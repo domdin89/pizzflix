2ed3b8720c02c2aca458951632dc87c993d6b0d3
Domenico Di Nardo <domenico.dnrd@hotmail.com>
2023-07-26 11:02:53 +0200

    init

--- app/urls.py
+++ app/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'app'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls'))
+    path('', views.home, name='home'),
 ]
--- pizzflix/settings.py
+++ pizzflix/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -156,7 +157,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'site_static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+#LOGIN_REDIRECT_URL = ''
+#LOGIN_URL = 'login'
